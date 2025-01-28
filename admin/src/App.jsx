c14e34dc5f95e9e1874e128510b89956fc9a0ada
lilia-g <liliagh2024@gmail.com>
2025-01-28 13:36:03 +0100

    router almost done in the client

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {BrowserRouter 
+  as Router,Routes, Route
+} from "react-router-dom";
+// import main from './components/main.jsx'
+// import login from './components/login.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,13 +25,32 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <div>
+      <Router>
+      <Routes>
+         {/* client */}
+        <Route path="/" element={<main/>} />
+        <Route path="/login" element={<login/>}/>
+        <Route path="/cart" element={<cart/>}/>
+        <Route path="/Wishlist" element={<wishlist/>}/>
+       {/* seller */}
+        <Route path="/listofproduct" element={<listofproduct/>}/>
+        <Route path="/add" element={<addproduct/>}/>
+        <Route path="/update" element={<updateproduct/>}/>
+        <Route path="/delete" element={<delete/>}/>
+      </Routes>
+      </Router>
+    </div>
+
+
+
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
