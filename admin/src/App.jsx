15637180881eacbde04682ebadfc6e966985ae6d
NourTebourski <nourtebourski96@gmail.com>
2025-01-29 14:49:45 +0100

    pull

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ListOfProducts from "./commponents/ListOfProducts";
 import ListOfUsers from "./commponents/ListOfUsers";
 import Navbar from "./commponents/Navbar.jsx";
 import Sidebar from "./commponents/Sidebar.jsx";
+import axios from "axios"
 function App() {
   const [View, setView] = useState("product");
   const [users,setusers] = useState([])
   const changeView = (view) => {
     setView(view);
   };
+  const fetch = ()=>{
+  axios.get("")
+  .then((res)=>{setusers(res.data)})
+  }
+  useEffect(()=>{
+    fetch()
+  },[])
+  console.log("data",data)
   return (
     <div>
      <Navbar changeView={changeView} />
