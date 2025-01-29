cfcbdd6bb2018797bd550d42d1ab52a048860a2b
houssem-saadbennani <saadbennanihoussem@gmail.com>
2025-01-29 16:51:36 +0100

    done

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -39,7 +39,7 @@ function Login() {
       if (user.role === "admin") {
         window.location.href = "http://localhost:5173/";
       } else if (user.role === "seller") {
-        navigate("/");
+        navigate("/main/seller");
       } else {
         navigate("/");
       }
