2e6cb37605351944b6964da6ead5668da3a4c0e6
NourTebourski <nourtebourski96@gmail.com>
2025-01-30 14:33:37 +0100

    toggle mta3 toggle

--- admin/src/commponents/Sidebar.jsx
+++ admin/src/commponents/Sidebar.jsx
@@ -18,10 +18,10 @@ function Sidebar({changeView,addCategory}) {
   <a className="active" href="#home">Admin Dashbord</a>
   <a href="#user" onClick={()=>changeView("user")}>List Users</a>
   <a href="#product" onClick={()=>changeView("product")}>List Product</a>
-  <a href="#categories" onClick={()=>changeView("categories")}>List Categories</a>
-  <a href="#" onClick={()=>{toggle("yes")}} >Add categorie</a>
-  {show==="yes"&&<input  className="form-control" type="text" value="Readonly input here..." aria-label="readonly input example" readOnly typeof="text" onChange={(e)=>{setname(e.target.value)}} />}
-  <div style={{display:"flex",justifyContent:"center",marginTop:"1px"}}><button type='button' className='btn btn-outline-success'   onClick={()=>{addCategory({name:name})}}>ADD</button></div>
+  <a href="#categories" onClick={()=>{changeView("categories"),toggle("wari")}}>List Categories</a>
+  {show==="wari"&&<a href="#" onClick={()=>{toggle("yes")}} >Add categorie</a>}
+  {show==="yes"&&<div style={{display:"flex",justifyContent:"center",marginTop:"5px"}}><input  className="form-control" type="text" placeholder='entre category name' aria-label="entre category name"  typeof="text" onChange={(e)=>{setname(e.target.value)}} /></div>}
+  {show==="yes"&&<div style={{display:"flex",justifyContent:"center",marginTop:"10px"}}><button type='button' className='btn btn-secondary'   onClick={()=>{addCategory({name:name})}}>ADD</button></div>}
   
 </div>
     </div>
